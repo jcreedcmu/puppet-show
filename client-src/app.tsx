8c3942f74e49298376adb21618f66de4324eddde
Jason Reed <jcreed@gmail.com>
2021-04-21 15:51:12 -0400

    some more canvas shenanigans

--- client-src/app.tsx
+++ client-src/app.tsx
@@ -1,26 +1,48 @@
 import { h, render, JSX } from 'preact';
 import { useState, useRef, useEffect } from 'preact/hooks';
 
-type State = { s: string };
+type Point = { x: number, y: number };
+type Actor = { p: Point, msg: string, color: string };
+type State = { actors: Actor[] };
 
-function useCanvas(msg: string) {
+const WIDTH = 640;
+const HEIGHT = 480;
+
+function relpos<T extends HTMLElement>(event: JSX.TargetedMouseEvent<T>): Point {
+  const rect = event.currentTarget.getBoundingClientRect();
+  return {
+    x: event.pageX - rect.left,
+    y: event.pageY - rect.top
+  };
+}
+
+function useCanvas(state: State) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   useEffect(() => {
-    console.log('rendering');
     const canvas = canvasRef.current;
-    const d = canvas.getContext('2d')!;
-    d.clearRect(0, 0, 100, 100);
-    d.fillStyle = 'black';
-    d.fillRect(0, 0, 100, 100);
-    d.fillStyle = 'white';
-    d.fillText(msg, 20, 20);
-  }, [msg]);
+    if (canvas != null) {
+      const d = canvas.getContext('2d')!;
+      d.clearRect(0, 0, WIDTH, HEIGHT);
+
+      state.actors.forEach(actor => {
+        const { p, msg, color } = actor;
+        d.fillStyle = color;
+        d.fillRect(p.x - 20, p.y - 20, 40, 40);
+        d.fillText(msg, p.x + 50, p.y);
+      });
+    }
+  }, [state]);
   return { canvasRef };
 }
 
-/** @jsx h */
+function hitTest(actor: Actor, p: Point) {
+  return (p.x < actor.p.x + 20 && p.x > actor.p.x - 20 &&
+    p.y < actor.p.y + 20 && p.y > actor.p.y - 20);
+}
+
 export const App = () => {
   const [input, setInput] = useState<string>('...');
+  const [cursor, setCursor] = useState<boolean>(false);
   const style: JSX.CSSProperties = {
     border: 'none',
     background: '#def',
@@ -28,20 +50,38 @@ export const App = () => {
     fontSize: 24,
     fontFamily: 'courier new',
   };
-  const { canvasRef } = useCanvas(input);
-  const style2 = { ...style, fontFamily: "sans-serif" };
+  const state: State = {
+    actors: [
+      { color: 'red', msg: 'hello', p: { x: 40, y: 50 } },
+      { color: 'blue', msg: 'world', p: { x: 100, y: 100 } }
+    ]
+  }
+  const { canvasRef } = useCanvas(state);
+
   const div1 =
     <div>
       <input size={40} spellcheck={false} style={style} value={input} onInput={e =>
         setInput(e.currentTarget.value)} />
     </div>;
-  const div2 =
-    <div>
-      <input size={40} spellcheck={false} style={style2} value={input} onInput={e =>
-        setInput(e.currentTarget.value)} />
-    </div>;
-  const canvas = <canvas ref={canvasRef} />
-  return <span>{div1}{div2}{canvas}</span>;
+
+  const onMouseMove: (e: JSX.TargetedMouseEvent<HTMLCanvasElement>) => void = (e) => {
+    const p = relpos(e);
+    for (const actor of state.actors) {
+      if (hitTest(actor, p)) {
+        setCursor(true);
+        return;
+      }
+    }
+    setCursor(false);
+  };
+  const canvasProps = {
+    style: { cursor: cursor ? 'pointer' : undefined, border: '1px solid black' },
+    width: WIDTH,
+    height: HEIGHT,
+    ref: canvasRef,
+    onMouseMove
+  };
+  return <span><canvas {...canvasProps} /><br /><br />{div1}</span>;
 }
 
 export function run() {
