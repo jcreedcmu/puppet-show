3f2ad68b06112b63f85e0847d944655367a31e66
Jason Reed <jcreed@gmail.com>
2021-04-21 14:51:36 -0400

    different plumbing also works

--- client-src/app.tsx
+++ client-src/app.tsx
@@ -3,23 +3,24 @@ import { useState, useRef, useEffect } from 'preact/hooks';
 
 type State = { s: string };
 
-function useCanvas() {
-  const [state, setState] = useState<State>({ s: 'hello' });
+function useCanvas(msg: string) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   useEffect(() => {
+    console.log('rendering');
     const canvas = canvasRef.current;
     const d = canvas.getContext('2d')!;
     d.clearRect(0, 0, 100, 100);
     d.fillStyle = 'black';
     d.fillRect(0, 0, 100, 100);
     d.fillStyle = 'white';
-    d.fillText(state.s, 20, 20);
-  });
-  return { canvasRef, state, setState };
+    d.fillText(msg, 20, 20);
+  }, [msg]);
+  return { canvasRef };
 }
 
 /** @jsx h */
 export const App = () => {
+  const [input, setInput] = useState<string>('...');
   const style: JSX.CSSProperties = {
     border: 'none',
     background: '#def',
@@ -27,17 +28,17 @@ export const App = () => {
     fontSize: 24,
     fontFamily: 'courier new',
   };
-  const { canvasRef, state, setState } = useCanvas();
+  const { canvasRef } = useCanvas(input);
   const style2 = { ...style, fontFamily: "sans-serif" };
   const div1 =
     <div>
-      <input size={40} spellcheck={false} style={style} value={state.s} onInput={e =>
-        setState({ s: e.currentTarget.value })} />
+      <input size={40} spellcheck={false} style={style} value={input} onInput={e =>
+        setInput(e.currentTarget.value)} />
     </div>;
   const div2 =
     <div>
-      <input size={40} spellcheck={false} style={style2} value={state.s} onInput={e =>
-        setState({ s: e.currentTarget.value })} />
+      <input size={40} spellcheck={false} style={style2} value={input} onInput={e =>
+        setInput(e.currentTarget.value)} />
     </div>;
   const canvas = <canvas ref={canvasRef} />
   return <span>{div1}{div2}{canvas}</span>;
